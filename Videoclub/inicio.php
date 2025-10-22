8f2bc14420fce5a06825defa64171bc512f07bb8
anpeca <148550074+anpeca@users.noreply.github.com>
2025-10-22 10:45:26 +0200

    ejercicio331 completado.

---
+++ Videoclub/inicio.php
@@ -0,0 +1,17 @@
+<?php
+require_once "app/Dwes/ProyectoVideoclub/Soporte.php";
+require_once "app/Dwes/ProyectoVideoclub/CintaVideo.php";
+require_once "app/Dwes/ProyectoVideoclub/Dvd.php";
+require_once "app/Dwes/ProyectoVideoclub/Juego.php";
+
+use Dwes\ProyectoVideoclub\Soporte;
+use Dwes\ProyectoVideoclub\CintaVideo;
+use Dwes\ProyectoVideoclub\Dvd;
+use Dwes\ProyectoVideoclub\Juego;
+
+// El código de prueba sigue igual...
+$soporte1 = new Soporte("Tenet", 22, 3);
+$miCinta = new CintaVideo("Los cazafantasmas", 23, 3.5, 107);
+$miDvd = new Dvd("Origen", 24, 15, "es,en,fr", "16:9");
+$miJuego = new Juego("The Last of Us Part II", 26, 49.99, "PS4", 1, 1);
+?>
---
+++ Videoclub/inicio2.php
@@ -0,0 +1,15 @@
+<?php
+require_once "app/Dwes/ProyectoVideoclub/CintaVideo.php";
+require_once "app/Dwes/ProyectoVideoclub/Dvd.php";
+require_once "app/Dwes/ProyectoVideoclub/Juego.php";
+require_once "app/Dwes/ProyectoVideoclub/Cliente.php";
+
+use Dwes\ProyectoVideoclub\CintaVideo;
+use Dwes\ProyectoVideoclub\Dvd;
+use Dwes\ProyectoVideoclub\Juego;
+use Dwes\ProyectoVideoclub\Cliente;
+
+// El código de prueba sigue igual...
+$cliente1 = new Cliente("Bruce Wayne", 23);
+$soporte1 = new CintaVideo("Los cazafantasmas", 23, 3.5, 107);
+?>
